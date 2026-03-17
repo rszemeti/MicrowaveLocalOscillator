30a367011efcff1361250d9cd55111e72d0ba4b3
Robin Szemeti <46870724+rszemeti@users.noreply.github.com>
2026-03-17 10:29:11 +0000

    misc

--- Firmware/Firmware.ino
+++ Firmware/Firmware.ino
@@ -3,24 +3,32 @@
 #include <tinySPI.h>
 #include <avr/sleep.h>
 
-#define ADF4001 DDB4      // 4001                
-#define ADF4153 DDB3      // 4153        
+#define ADF4001  PB3      // 4001      - 10MHz      
+#define ADF4001A PB4      // 4001      - 10MHz   
+#define ADF4001B PB0      // 4001      - 10MHz       
+       
 
 
-uint32_t Reg4001[5] =  {
+uint32_t Reg4001[3] =  {
   0x1FC092, 
   0x100004, 
   0x000101,
 } ; // 10Mhz oscillator, ref 10MHz, Muxout: digital lock detect
 
-uint32_t Reg4153[6] =  {
-  0x000003, /* clear spurs mode R3*/
-  0x3C7,    /* write desired spurs mode R3*/
-  0x1386,   /* Enable counter reset R2 =R2+4 */
-  0x104015,  /* Write R1 */
-  0x3E0008, /* Write R0 */
-  0x1382    /* Disable counter reset  R2*/
- } ; 
+
+uint32_t Reg4001A[3] =  {
+  0x1FC092, 
+  0x100004, 
+  0x000101,
+} ; // 1st VHF oscillator
+
+
+uint32_t Reg4001C[3] =  {
+  0x1FC092, 
+  0x100004, 
+  0x000101,
+} ; // 2nd VHF Oscillatr
+
 
 void setup() {
   SPI.begin();
@@ -28,15 +36,20 @@ void setup() {
   pinMode(ADF4001, OUTPUT);
   digitalWrite(ADF4001, LOW);
 
-  pinMode(ADF4153, OUTPUT);
-  digitalWrite(ADF4153, LOW);
+  pinMode(ADF4001A, OUTPUT);
+  digitalWrite(ADF4001A, LOW);
+
+  pinMode(ADF4001B, OUTPUT);
+  digitalWrite(ADF4001A, LOW);
+
 
   delay(500);
   ADFSet(Reg4001, 3, ADF4001);
   delay(500);
-  ADFSet(Reg4153, 6, ADF4153);
+  ADFSet(Reg4001, 3, ADF4001A);
   delay(500);
-
+  ADFSet(Reg4001, 3, ADF4001B);
+  delay(500);
 
 }
 
@@ -48,18 +61,14 @@ void loop() {
   sleep_mode();
 }
 
-void ADFSet(uint32_t reg[], int c, byte LE) {
-  for (int i = 0; i < c; i++) {
-    ADFWriteRegister32(reg[i], LE);
-  }
-}
-
-void ADFWriteRegister32(const uint32_t value, byte LE) //Program data into one of the 32bits register, the register that is selected by the four LSBs.
+void ADFWriteRegister32(const uint32_t value, byte LE) 
 {
   digitalWrite(LE, LOW);
-  for (int i = 3; i >= 0; i--)               // loop through 4 x 8bits
-    SPI.transfer((value >> 8 * i) & 0xFF);   // shift , mask and send octet via SPI
-  digitalWrite(LE, HIGH);                    // Load Enable rising edge the data stored in the shift register
+  for (int i = 3; i >= 0; i--) {             
+    byte byteToSend = (value >> (i * 8)) & 0xFF;
+    SPI.transfer(byteToSend);
+  }
+  digitalWrite(LE, HIGH);                    
   delay(1);
-  digitalWrite(LE, LOW);                     //
+  digitalWrite(LE, LOW);                     
 }
